726b62c97ad056741ab8b3432b76b965a03904f2
setouchi-h <setouchi.kazuki@gmail.com>
2023-04-16 01:39:35 +0900

    deploy other networks

--- contract/contracts/AyAyPaymaster.sol
+++ contract/contracts/AyAyPaymaster.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import "@account-abstraction/contracts/core/BasePaymaster.sol";
 import "./interfaces/IAyAyReceiver.sol";
 import "./interfaces/IAyAyWallet.sol";
--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,50 +1,67 @@
-import { HardhatUserConfig } from "hardhat/config"
-import "dotenv/config"
-import "@nomicfoundation/hardhat-toolbox"
-import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
 
 const accounts = {
-  mnemonic: process.env.DEV_MNEMONIC || ""
-}
+  mnemonic: process.env.DEV_MNEMONIC || "",
+};
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.18",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 2000
-      }
-    }
+        runs: 2000,
+      },
+    },
   },
   namedAccounts: {
     deployer: {
-      default: 0
+      default: 0,
     },
     shop1: {
-      default: 1
+      default: 1,
     },
     consumer1: {
-      default: 2
-    }
+      default: 2,
+    },
   },
   defaultNetwork: "hardhat",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545",
-      accounts
+      accounts,
     },
     mumbai: {
       url: process.env.RPC_URL_MUMBAI || "",
-      accounts
+      accounts,
+    },
+    scrollAlpha: {
+      url: process.env.RPC_URL_SCROLLALPHA || "",
+      accounts,
+    },
+    linea: {
+      url: process.env.RPC_URL_LINEA || "",
+      accounts,
+    },
+    gnosis: {
+      url: process.env.RPC_URL_GNOSIS || "",
+      accounts,
+    },
+    chiado: {
+      url: process.env.RPC_URL_CHIADO || "",
+      gasPrice: 1000000000,
+      accounts,
     },
   },
   external: {
     contracts: [
       {
-        artifacts: "node_modules/@account-abstraction/contracts/artifacts"
-      }
-    ]
-  }
-}
+        artifacts: "node_modules/@account-abstraction/contracts/artifacts",
+      },
+    ],
+  },
+};
 
-export default config
+export default config;
