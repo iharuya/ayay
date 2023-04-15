f0058b8991e927175eff5feb041e5d736ce9dadb
iharuya <i4haruya@gmail.com>
2023-04-15 18:15:11 +0900

    change to faucet

--- server/pages/api/faucet.ts
+++ server/pages/api/faucet.ts
@@ -6,18 +6,18 @@ import { testJpyc } from "@/lib/contracts"
 import { ethers } from "ethers"
 
 const PostSchema = z.object({
-  body: z.object({ address: ethAddress.transform(toChecksumAddress) })
+  query: z.object({ address: ethAddress.transform(toChecksumAddress) })
 })
 
 const handlePost = withZod(PostSchema, async (req, res) => {
-  testJpyc.transfer(req.body.address, ethers.utils.parseUnits("1000", 6))
-  return res.status(200).json({message: "1,000 JPYC deposit in queue..."})
+  await testJpyc.transfer(req.query.address, ethers.utils.parseUnits("5000", 6))
+  return res.status(200).json({message: "5,000 JPYC deposit in queue..."})
 })
 
 const handler: NextApiHandler = async (req, res) => {
   await runMiddleware(req, res)
   switch (req.method) {
-    case "POST":
+    case "GET":
       return handlePost(req, res)
     default:
       return res.status(405).json({ message: "Method not allowed" })
