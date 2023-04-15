7f8da03feca6e04ebb3a5a8ed34e57d38a1a80b3
setouchi-h <setouchi.kazuki@gmail.com>
2023-04-16 03:37:20 +0900

    eth tokyo

--- contract/contracts/AyAyReceiver.sol
+++ contract/contracts/AyAyReceiver.sol
@@ -2,15 +2,17 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IAyAyReceiver.sol";
 
 // No ETH deposit allowed
 contract AyAyReceiver is IAyAyReceiver, Ownable {
     IERC20 public immutable CURRENCY;
-    constructor(address owner, IERC20 currency) {
+
+    constructor(address owner, address currency) {
         _transferOwnership(owner);
-        CURRENCY = currency;
+        CURRENCY = ERC20(currency);
     }
 
     function currencyAddress() public view returns (address) {
